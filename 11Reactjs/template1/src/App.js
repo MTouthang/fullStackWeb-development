a97b881998f569f4d1472f7b2d338c2278225cd2
Mamang touthang <huebart6@gmail.com>
2022-06-25 14:41:31 +0530

    context api theme switcher

---
+++ 11Reactjs/template1/src/App.js
@@ -0,0 +1,19 @@
+import AppSection from "./AppSection";
+import CardSection from "./CardSection";
+import Footer from "./Footer";
+import HeroSection from "./HeroSection";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+      <AppSection />
+      <CardSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
